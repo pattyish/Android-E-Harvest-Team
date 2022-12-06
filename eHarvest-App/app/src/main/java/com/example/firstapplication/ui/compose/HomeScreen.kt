18c8d33517a5a1a707499384525fa5e65f8bcf16
pattyish <patrickishimwe10@gmail.com>
2022-12-06 15:44:48 +0200

    resolving confslict

--- eHarvest-App/app/src/main/java/com/example/firstapplication/ui/compose/HomeScreen.kt
+++ eHarvest-App/app/src/main/java/com/example/firstapplication/ui/compose/HomeScreen.kt
@@ -90,7 +90,7 @@ fun CombinedTabWithCustomIndicator() {
 //            }
         val items = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13", "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20", "Item 21", "Item 22")
         val items2 = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13", "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20", "Item 21", "Item 22")
-    val items3 = listOf("4343434", "21121")
+        val items3 = listOf("4343434", "21121")
 
         if (tabIndex == 0) {
             StringGrid(items = items)
